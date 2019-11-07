bdb1cd9feecc190415a74cfbb47dfc09ab3a2f55
Gabriel Alves <you@example.com>
2019-11-06 21:04:09 -0300

    Criada pasta da aula 8

---
+++ Aula8/js/scripts.js
@@ -0,0 +1,14 @@
+let meNome = ["Erick", "Alexandre", "Mendes", "de", "Souza"];
+
+let i = 4;
+
+for(let indice = 0; indice < meNome.length; indice++){
+    console.log(meNome[indice]);
+}
+
+console.log(" ");
+
+while(i > meNome.length - meNome.length + -1){
+    console.log(meNome[i]);
+    i--;
+}
