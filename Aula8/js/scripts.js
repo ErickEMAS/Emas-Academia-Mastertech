debadc3d93949bbf68fec0988e61e9b0cb0253d2
Gabriel Alves <you@example.com>
2019-11-06 21:52:34 -0300

    Iniciado site da escada

--- Aula8/js/scripts.js
+++ Aula8/js/scripts.js
@@ -1,3 +1,4 @@
+/*
 let meNome = ["Erick", "Alexandre", "Mendes", "de", "Souza"];
 
 let i = 4;
@@ -11,4 +12,13 @@ console.log(" ");
 while(i > meNome.length - meNome.length + -1){
     console.log(meNome[i]);
     i--;
+}*/
+
+let material = "#"//prompt("Escolha o material da escada");
+let quantidade = 5//Number(prompt("Escolha a quantidade de dregaus"));
+
+for(let i = 0; i < quantidade; i++){
+    let escada = ["a", "b"];
+    escada.push(material);
+    console.log(escada[]);
 }
