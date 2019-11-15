3910e1e2cab96585c5dd76b5e569d5d3d26fcfaa
ErickEMAS <erick8alexandre@gmail.com>
2019-11-14 21:56:46 -0300

    Finalizada atividade /sobre

--- Aula14/Erick/views.py
+++ Aula14/Erick/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mostrar_index(request):
+    return render(request, 'index.html')
+
+def mostrar_sobre(resquest):
+    return render(resquest, 'sobre.html')
--- Aula14/emas/urls.py
+++ Aula14/emas/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Erick import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mostrar_index),
+    path('sobre/', views.mostrar_sobre)
 ]
